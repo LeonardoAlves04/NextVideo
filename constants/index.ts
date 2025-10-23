495bf951d2e9b83b7b0173aad1b5554136fd6023
Leonardo Alves <leonardoalves201510@gmail.com>
2025-10-23 14:44:17 -0300

    feat:add sidebar component

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks = [
+  {
+    label: "Home",
+    route: "/",
+    imageUrl: "/icons/home.svg",
+  },
+  {
+    label: "Upcoming",
+    route: "/upcoming",
+    imageUrl: "/icons/upcoming.svg",
+  },
+  {
+    label: "Previous    ",
+    route: "/previous",
+    imageUrl: "/icons/previous.svg",
+  },
+  {
+    label: "Recordings",
+    route: "/Recordings",
+    imageUrl: "/icons/recordings.svg",
+  },
+  {
+    label: "Personal Room",
+    route: "/personal-room",
+    imageUrl: "/icons/personal-room.svg",
+  },
+];
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,37 @@
+"use client";
+import Link from "next/link";
+import { sidebarLinks } from "../../constants";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+
 const Sidebar = () => {
+  const pathname = usePathname();
+
   return (
-    <section className="bg-dark-1 p-4 text-white">
-      <h2 className="text-lg font-semibold mb-2">Sidebar</h2>
-      <p className="text-sm">Testando bg-dark-1</p>
+    <section
+      className="bg-dark-1 p-4 text-white sticky left-0 top-0 flex h-screen w-fit flex-col justify-between
+    pt-28 p-6 max-sm:hidden lg:w-[264px]"
+    >
+      <div className="flex flex- flex-col gap-6">
+        {sidebarLinks.map((link) => {
+          const isActive =
+            pathname === link.route || pathname.startsWith(link.route);
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className={cn(
+                "flex items-center gap-4 p-4 rounded-lg justify-start",
+                {
+                  "bg-blue-1": isActive,
+                }
+              )}
+            >
+              {link.label}
+            </Link>
+          );
+        })}
+      </div>
     </section>
   );
 };
