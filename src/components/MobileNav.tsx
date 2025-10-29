1b3a5e366fc5592f217d452b72fa0cf884972cb9
Leonardo Alves de Almeida <leonardoalves201510@gmail.com>
2025-10-28 22:32:36 -0300

    fix: downgrade next version to 14 for stop incompatibilities

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -9,8 +7,8 @@ import {
   SheetClose,
   SheetContent,
   SheetTrigger,
-  SheetTitle,
 } from "@/components/ui/sheet";
+
 import { cn } from "@/lib/utils";
 import { sidebarLinks } from "../../constants";
 
@@ -30,7 +28,6 @@ const MobileNav = () => {
           />
         </SheetTrigger>
         <SheetContent side="left" className="border-none bg-dark-1">
-          <SheetTitle className="sr-only">Menu de Navegação</SheetTitle>
           <Link href="/" className="flex items-center gap-1">
             <Image
               src="/icons/logo.svg"
@@ -41,33 +38,36 @@ const MobileNav = () => {
             <p className="text-[26px] font-extrabold text-white">YOOM</p>
           </Link>
           <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
-            <section className="flex h-full flex-col gap-6 pt-16 text-white">
-              {sidebarLinks.map((item) => {
-                const isActive = pathname === item.route;
+            <SheetClose asChild>
+              <section className=" flex h-full flex-col gap-6 pt-16 text-white">
+                {sidebarLinks.map((item) => {
+                  const isActive = pathname === item.route;
 
-                return (
-                  <SheetClose asChild key={item.route}>
-                    <Link
-                      href={item.route}
-                      className={cn(
-                        "flex gap-4 items-center p-4 rounded-lg w-full max-w-60",
-                        {
-                          "bg-blue-1": isActive,
-                        }
-                      )}
-                    >
-                      <Image
-                        src={item.imageURL}
-                        alt={item.label}
-                        width={20}
-                        height={20}
-                      />
-                      <p className="font-semibold">{item.label}</p>
-                    </Link>
-                  </SheetClose>
-                );
-              })}
-            </section>
+                  return (
+                    <SheetClose asChild key={item.route}>
+                      <Link
+                        href={item.route}
+                        key={item.label}
+                        className={cn(
+                          "flex gap-4 items-center p-4 rounded-lg w-full max-w-60",
+                          {
+                            "bg-blue-1": isActive,
+                          }
+                        )}
+                      >
+                        <Image
+                          src={item.imageURL}
+                          alt={item.label}
+                          width={20}
+                          height={20}
+                        />
+                        <p className="font-semibold">{item.label}</p>
+                      </Link>
+                    </SheetClose>
+                  );
+                })}
+              </section>
+            </SheetClose>
           </div>
         </SheetContent>
       </Sheet>
