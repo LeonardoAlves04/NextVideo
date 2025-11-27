b493435d44fa469daf4572976d57d2e8b824d5eb
Leonardo Alves de Almeida <leonardoalves201510@gmail.com>
2025-11-27 20:20:15 -0300

    feat: add clerk authentication

--- src/app/(root)/(home)/layout.tsx
+++ src/app/(root)/(home)/layout.tsx
@@ -3,6 +3,7 @@ import { ReactNode } from "react";
 
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: "YOOM",
@@ -11,17 +12,19 @@ export const metadata: Metadata = {
 
 const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
   return (
-    <main className="relative">
-      <Navbar />
+    <ClerkProvider>
+      <main className="relative">
+        <Navbar />
 
-      <div className="flex">
-        <Sidebar />
+        <div className="flex">
+          <Sidebar />
 
-        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
-          <div className="w-full">{children}</div>
-        </section>
-      </div>
-    </main>
+          <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
+            <div className="w-full">{children}</div>
+          </section>
+        </div>
+      </main>
+    </ClerkProvider>
   );
 };
 
