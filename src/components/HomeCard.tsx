1ea8b665d0885710f51982e9623f4e02c937e348
Leonardo Alves de Almeida <leonardoalves201510@gmail.com>
2026-02-01 17:26:59 -0300

    feat:add home-page cards

---
+++ src/components/HomeCard.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+
+interface HomeCardProps {
+  className: string;
+  img: string;
+  title: string;
+  description: string;
+  handleClick: () => void;
+}
+
+const HomeCard = ({
+  className,
+  img,
+  title,
+  description,
+  handleClick,
+}: HomeCardProps) => {
+  return (
+    <div
+      className={cn(
+        "px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer",
+        className,
+      )}
+      onClick={() => {
+        handleClick;
+      }}
+    >
+      <div className="flex-center glassmorphism size-12 rounded-[10px]">
+        <Image
+          src="/icons/add-meeting.svg"
+          alt="video call"
+          width={24}
+          height={24}
+        />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <h1 className="text-2xl font-bold">Nova reunião</h1>
+        <p className="text-lg font-normal">Comece uma nova reunião</p>
+      </div>
+    </div>
+  );
+};
+
+export default HomeCard;
--- src/components/MeetingTypeList.tsx
+++ src/components/MeetingTypeList.tsx
@@ -1,9 +1,47 @@
-import React from 'react'
+"use client";
+
+import React, { useState } from "react";
+import HomeCard from "./HomeCard";
+import { useRouter } from "next/navigation";
 
 const MeetingTypeList = () => {
-  return (
-    <div>MeetingTypeList</div>
-  )
-}
+  const router = useRouter();
+  const [meetingType, setMeetingType] = useState<
+    "isScheduleMeeting" | "isInstantMeeting" | "isJoiningMeeting" | undefined
+  >();
 
-export default MeetingTypeList
+  return (
+    <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
+      <HomeCard
+        img="/icons/add-meeting.svg"
+        title="Nova reunião"
+        description="Comece uma nova reunião"
+        handleClick={() => setMeetingType("isJoiningMeeting")}
+        className="bg-orange-1"
+      />
+      <HomeCard
+        img="/icons/schedule-meeting.svg"
+        title="Agendar reunião"
+        description="Agende uma nova reunião"
+        handleClick={() => setMeetingType("isScheduleMeeting")}
+        className="bg-blue-1"
+      />
+      <HomeCard
+        img="/icons/recordings.svg"
+        title="Ver gravações"
+        description="Veja suas gravações"
+        handleClick={() => router.push("/recordings")}
+        className="bg-purple-1"
+      />
+      <HomeCard
+        img="/icons/add-meeting.svg"
+        title="Nova reunião"
+        description="Comece uma nova reunião"
+        handleClick={() => setMeetingType("isJoiningMeeting")}
+        className="bg-yellow-1"
+      />
+    </section>
+  );
+};
+
+export default MeetingTypeList;
